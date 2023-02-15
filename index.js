537b4e299a9f00965f528ba52f7c194f361fcf33
Luferat <contato@luferat.net>
2023-02-15 09:59:03 -0300

    Ajustes POST e PUT

--- index.js
+++ index.js
@@ -7,9 +7,47 @@ console.log(conf);
 
 const port = conf.HTTPPORT;
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo doido!')
-})
+// Objeto que será executado quando houver uma requisição.
+const controller = {
+  resJson: async (req, res) => {
+
+    // Lista com alguns atributos úteis da requisição (req) HTTP.
+    data = {
+      "method": req.method,
+      "url": req.url,
+      "baseURL": req.baseURL,
+      "query":res.query,
+      "params": req.params,
+      "body": req.body,
+      "headers": req.headers
+    }
+
+    // Envia JSON com os dados acima para o cliente, como texto plano.
+    // res.send(data);
+
+    // Envia JSON com os dados acima para o cliente, como texto plano JSON.
+    res.json(data);
+    
+  }
+}
+
+// Recebe os dados do body HTTP e valida em JSON → POST e PUT.
+const bodyParser = require('body-parser').json();
+
+// Rota para GET → getAll() → Recebe, por exemplo, todos os registros.
+app.get('/', controller.resJson);
+
+// Rota para GET → get(id) → Recebe apenas o registro identificado.
+app.get('/:id', controller.resJson);
+
+// Rota para DELETE.
+app.delete('/:id', controller.resJson);
+
+// Rota para POST → bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.post('/', bodyParser, controller.resJson);
+
+// Rota para PUT → bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.put('/:id', bodyParser, controller.resJson);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
