e5f87a8907d534c523a36f472c25ba178525c03f
Luferat <contato@luferat.net>
2023-02-17 10:48:32 -0300

    MySQL Ok!

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-
 const conf = require('dotenv').config().parsed;
+// console.log(conf);
 
-console.log(conf);
+// Importa a biblioteca "MySQL".
+const mysql = require('mysql2');
+
+// Faz a conexão com o servidor.
+const conn = mysql.createPool({
+  host: conf.HOSTNAME,
+  user: conf.USERNAME,
+  database: conf.DATABASE,
+  password: conf.PASSWORD,
+  port: conf.HOSTPORT
+}).promise();
 
 const port = conf.HTTPPORT;
 
@@ -11,46 +21,133 @@ const port = conf.HTTPPORT;
 const thing = {
 
   getAll: async (req, res) => {
-    res.json({
-      "req": req.method,
-      "status": "ok"
-    });
+
+    try {
+
+      // Executa query que obtém todos os registros.
+      const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tstatus = 'on' ORDER BY tdate DESC";
+      const [rows] = await conn.query(sql);
+
+      // View dos dados.
+      res.json({ data: rows });
+
+    }
+    catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
+
   },
 
   getOne: async (req, res) => {
-    const id = req.params.id;
-    res.json({
-      "req": req.method,
-      "id": req.params.id,
-      "status": "ok"
-    });
+
+    try {
+
+      // Id da requisição.
+      const id = req.params.id;
+
+      // Executa query que obtem um registro único.
+      const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tid = ? AND tstatus = 'on' ORDER BY tdate DESC";
+      const [rows] = await conn.query(sql, [id]);
+
+      // View dos dados.
+      res.json({ coisa: rows });
+
+    } catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
+
   },
 
   post: async (req, res) => {
-    res.json({
-      "req": req.method,
-      "data": req.body,
-      "status": "ok"
-    });
+
+    try {
+
+      // Extrai os campos do req.body.
+      const { user, name, photo, description, location, options } = req.body;
+
+      //////////////////////////////////////////////
+      // A validação dos dados será feita aqui!!! //
+      //////////////////////////////////////////////
+
+      // Executa query de inserção do registro.
+      const sql = "INSERT INTO things (tuser, tname, tphoto, tdescription, tlocation, toptions) VALUES (?, ?, ?, ?, ?, ?)";
+      const [rows] = await conn.query(sql, [user, name, photo, description, location, options]);
+
+      // View do feedback.
+      res.json({ id: rows.insertId, status: "success" });
+
+    } catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
+
   },
 
   put: async (req, res) => {
-    res.json({
-      "req": req.method,
-      "id": req.params.id,
-      "data": req.body,
-      "status": "ok"
-    });
+
+    try {
+
+      // Id da requisição.
+      const id = req.params.id;
+
+      // Extrai os campos do req.body.
+      const { user, name, photo, description, location, options } = req.body;
+
+      //////////////////////////////////////////////
+      // A validação dos dados será feita aqui!!! //
+      //////////////////////////////////////////////
+
+      // Executa query de atualização do registro.
+      const sql = "UPDATE things SET tuser = ?, tname = ?, tphoto = ?, tdescription = ?, tlocation = ?, toptions = ? WHERE tid = ? AND tstatus = 'on'";
+      const [rows] = await conn.query(sql, [user, name, photo, description, location, options, id]);
+
+      // View do feedback.
+      res.json({ id: id, status: "success" });
+
+    } catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
 
   },
 
   delete: async (req, res) => {
-    res.json({
-      "req": req.method,
-      "id": req.params.id,
-      "status": "ok"
-    });
+
+    try {
+
+      // Id da requisição
+      const id = req.params.id;
+
+      //////////////////////////////////////////////
+      // A confirmação da ação será feita aqui!!! //
+      //////////////////////////////////////////////
+
+      // Executa query de atualização do status do registro.
+      const sql = "UPDATE things SET tstatus = 'del' WHERE tid = ?";
+      const [rows] = await conn.query(sql, [id]);
+
+      // View do feedback.
+      res.json({ id: id, status: "success" });
+
+    } catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
+
   }
+
 }
 
 // Objeto que trata requisições para o 'user'.
